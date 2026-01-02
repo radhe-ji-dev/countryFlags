80964c5e16ca2868abd609be76eed3569c8acc3e
Radhakant Tiwadi <69426689+radhe-ji-dev@users.noreply.github.com>
2026-01-02 13:35:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import CountryFlags from "./countryFlags";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CountryFlags />
+    </div>
+  );
+}
